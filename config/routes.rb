4ca7e6efdbc0b899d7dad42952482238f0be3510
marschine <martin_h_85@gmx.de>
2011-12-14 10:41:33 +0100

    nix gemacht nur ein test

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 SampleApp::Application.routes.draw do
 
+  get "users/new"
+
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
